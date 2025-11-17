ac1ffe8b694b5654fe4e07f282f6f95d1742660c
Harsh <harshjogel173@gmail.com>
2025-11-17 12:40:28 +0530

    Add files via upload

---
+++ sizeof.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+// #include <limits.h>
+// #include <float.h>
+
+int main()
+{
+    printf("size of int=%zu\n", sizeof(int));
+    printf("size of char=%zu\n", sizeof(char));
+    printf("size of float=%zu\n", sizeof(float));
+    printf("size of int=%zu\n", sizeof(double));
+}
