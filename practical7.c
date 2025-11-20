766fa56dfa665107481ad986ead6be0e9d22bfd5
Harsh <harshjogel173@gmail.com>
2025-11-20 10:05:01 +0530

    Add files via upload

---
+++ practical7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int age;
+    printf("enter your age");
+    scanf("%d",&age);
+    if (age>=12 && age<=60)
+
+        printf("entry with fee=50");
+
+    else
+        printf("free entry :)");
+}
