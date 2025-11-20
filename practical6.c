673601a917115bd438a791a9cc972851f03aadbd
Harsh <harshjogel173@gmail.com>
2025-11-20 10:04:35 +0530

    Add files via upload

---
+++ practical6.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+int main ()
+{
+    float f,c;
+    printf("enter c*=");
+    scanf("%f",&c);
+    f=(c*1.8)+32;
+    printf("%f",f);
+}
