1f192fb43c8fd4939060cc386d76278521ee9fe5
Harsh <harshjogel173@gmail.com>
2025-11-20 09:56:23 +0530

    Add files via upload

---
+++ 25araay.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int s = 0, t, abs, n;
+    printf("enter a student:");
+    scanf("%d", &n);
+    int A[n - 1];
+    for (int i = 0; i < n - 1; i++)
+    {
+        scanf("%d", &A[i]);
+        s = s + A[i];
+    }
+    t = (n * (n + 1)) / 2;
+    abs = t - s;
+    printf("Absent number:%d", abs);
+}
